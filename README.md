2483a2799f7275199aff52a3073b39430bdc43ed
Cocytus45 <134197768+Cocytus45@users.noreply.github.com>
2023-06-08 08:44:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Link-Checker
+ Checks links for U+2044, U+2215, .zip, and .mov. If detected it will prompt with a warning of the detected issues and request confirmation to proceed.
---
+++ src/contents.js
@@ -0,0 +1,78 @@
+function confirmNavigation(e) {
+    const target = e.target;
+    const decodedHref = decodeURIComponent(target.href || '');
+  
+    const containsU2044 = /\u2044/.test(decodedHref);
+    const containsU2215 = /\u2215/.test(decodedHref);
+    const containsZipWithAt = /@.*\.zip/.test(decodedHref);
+    const containsZipAlone = /(^|[^@])\.zip/.test(decodedHref);
+    const containsMovWithAt = /@.*\.mov/.test(decodedHref);
+    const containsMovAlone = /(^|[^@])\.mov/.test(decodedHref);
+  
+    const containsTextU2044 = /\u2044/.test(target.textContent);
+    const containsTextU2215 = /\u2215/.test(target.textContent);
+    const containsTextZipWithAt = /@.*\.zip/.test(target.textContent);
+    const containsTextZipAlone = /(^|[^@])\.zip/.test(target.textContent);
+    const containsTextMovWithAt = /@.*\.mov/.test(target.textContent);
+    const containsTextMovAlone = /(^|[^@])\.mov/.test(target.textContent);
+  
+    const shouldConfirm =
+      containsU2044 ||
+      containsU2215 ||
+      containsZipWithAt ||
+      containsZipAlone ||
+      containsMovWithAt ||
+      containsMovAlone ||
+      containsTextU2044 ||
+      containsTextU2215 ||
+      containsTextZipWithAt ||
+      containsTextZipAlone ||
+      containsTextMovWithAt ||
+      containsTextMovAlone;
+  
+    if (shouldConfirm) {
+      let message = '';
+  
+      if (containsU2044 || containsTextU2044) {
+        message += `- This link${
+          containsTextU2044 ? ' text' : ''
+        } contains U+2044 (⁄)\n`;
+      }
+  
+      if (containsU2215 || containsTextU2215) {
+        message += `- This link${
+          containsTextU2215 ? ' text' : ''
+        } contains U+2215 (∕)\n`;
+      }
+  
+      if (containsZipWithAt || containsTextZipWithAt) {
+        message += `- This link${
+          containsTextZipWithAt ? ' text' : ''
+        } contains an @ operator followed by .zip\n`;
+      } else if (containsZipAlone || containsTextZipAlone) {
+        message += `- This link${
+          containsTextZipAlone ? ' text' : ''
+        } contains .zip\n`;
+      }
+  
+      if (containsMovWithAt || containsTextMovWithAt) {
+        message += `- This link${
+          containsTextMovWithAt ? ' text' : ''
+        } contains an @ operator followed by .mov\n`;
+      } else if (containsMovAlone || containsTextMovAlone) {
+        message += `- This link${
+          containsTextMovAlone ? ' text' : ''
+        } contains .mov\n`;
+      }
+  
+      message += '\nAre you sure you would like to proceed?';
+  
+      if (!confirm(message)) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    }
+  }
+  
+  document.addEventListener('click', confirmNavigation, true);
+  
